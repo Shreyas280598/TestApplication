322423807a4a8f27a7473eac83a64f350b0a24e8
Shreyas280598 <shreyas.280598@gmail.com>
2023-08-15 17:38:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.testapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testapplication.ui.theme.TestApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        Text(
+            text = "Hello $name!",
+            fontSize = 30.sp,
+            modifier = Modifier.align(Alignment.BottomStart)
+
+        )
+        Spacer(modifier = Modifier.width(16.dp)) // A blank white space
+
+        Text(
+            text = "Hello test text!",
+            fontSize = 30.sp,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+
+    }
+
+}
